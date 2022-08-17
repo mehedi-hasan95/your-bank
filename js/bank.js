52a2d2a7687fb3f86219871865d35c81752319a3
mehedititu <mh.me.mehedi@gmail.com>
2022-08-17 16:34:22 +0600

    Update except validation

--- js/bank.js
+++ js/bank.js
@@ -1,8 +1,47 @@
 document.getElementById('deposit-btn').addEventListener('click', function() {
     const depositField = document.getElementById('deposit-field');
-    const depositAmmount = depositField.value;
+    const depositAmmountString = depositField.value;
+    const depositAmmount = parseFloat(depositAmmountString);
     
     const depositTotal = document.getElementById('deposit-total');
-    const depositTotalAmount = depositTotal.innerHTML;
-    depositTotal.innerHTML = depositAmmount;
+    const depositTotalAmountString = depositTotal.innerHTML;
+    const depositTotalAmount = parseFloat(depositTotalAmountString);
+    const totalAmount = depositTotalAmount + depositAmmount;
+    depositTotal.innerHTML = totalAmount;
+
+    // Total Ballence after add deposit 
+    const totalBalance = document.getElementById('total-balance');
+    const totalBalanceString = totalBalance.innerText;
+    const totalPrevBalance = parseFloat(totalBalanceString);
+    const totalPresentBalance = totalPrevBalance + depositAmmount;
+    totalBalance.innerText = totalPresentBalance
+
+    depositField.value = '';
+});
+
+document.getElementById('withdrow-btn').addEventListener('click', function() {
+    const withdrowField = document.getElementById('withdrow-field');
+    const withdrowFieldString = withdrowField.value;
+    const totalWithdrow = parseFloat(withdrowFieldString);
+
+    const withdrowTotal = document.getElementById('withdrow-total');
+    const withdrowTotalString = withdrowTotal.innerText;
+    const withdrowTotalAmmount = parseFloat(withdrowTotalString);
+
+    const withdrowAmmount = totalWithdrow + withdrowTotalAmmount;
+    withdrowTotal.innerText = withdrowAmmount;
+
+    // Total Ballence after withrow deposit 
+    const totalBalance = document.getElementById('total-balance');
+    const totalBalanceString = totalBalance.innerText;
+    const totalPrevBalance = parseFloat(totalBalanceString);
+
+
+    const totalPresentBalance = totalPrevBalance - totalWithdrow;
+    totalBalance.innerText = totalPresentBalance
+
+    
+    
+
+    withdrowField.value = '';
 });
